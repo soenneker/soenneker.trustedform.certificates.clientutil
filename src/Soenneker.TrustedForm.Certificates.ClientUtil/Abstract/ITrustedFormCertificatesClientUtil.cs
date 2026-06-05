09ede8773e8bbb27ed860ffca05ae6fe96b69936
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:55 -0500

    xml commenting

--- src/Soenneker.TrustedForm.Certificates.ClientUtil/Abstract/ITrustedFormCertificatesClientUtil.cs
+++ src/Soenneker.TrustedForm.Certificates.ClientUtil/Abstract/ITrustedFormCertificatesClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.TrustedForm.Certificates.ClientUtil.Abstract;
 /// </summary>
 public interface ITrustedFormCertificatesClientUtil : IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<TrustedFormCertificatesOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
